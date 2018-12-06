6ba6c03c30e51a1d857665e6b7d3aa35899ada14
Sergey Manzhak <manzhak.com@gmail.com>
2018-12-06 16:52:40 +0300

    Add degugbar

--- config/app.php
+++ config/app.php
@@ -160,6 +160,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
 
     ],
 
